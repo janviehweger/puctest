3a198587ee0f4c240d81303d403a3bb8992402d5
Jan Viehweger <forum@jan-viehweger.de>
2020-12-11 15:42:27 +0100

    added latest puc release code

--- puctest.php
+++ puctest.php
@@ -4,3 +4,10 @@
 * Plugin URI:        https://example.com/plugins/the-basics/
 * Description:       Handle the basics with this plugin.
 * Version:           1.0.0
+
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/janviehweger/puctest/',
+    __FILE__,
+    'puctest'
+);
