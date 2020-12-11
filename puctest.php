19fcbdc57fea5367996c89b4d7377c1a0032d450
Jan Viehweger <janviehweger@users.noreply.github.com>
2020-12-11 16:03:54 +0100

    Update puctest.php

--- puctest.php
+++ puctest.php
@@ -2,7 +2,7 @@
 /** 
 * Plugin Name:       puctest
 * Description:       testing puc.
-* Version:           2.0.0
+* Version:           3.0.0
 */
 
 require 'plugin-update-checker/plugin-update-checker.php';
