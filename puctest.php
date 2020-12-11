c1d5f0441b907d63ecd1bce75eac8623cc020db6
Jan Viehweger <janviehweger@users.noreply.github.com>
2020-12-11 15:35:47 +0100

    Create puctest.php

---
+++ puctest.php
@@ -0,0 +1,6 @@
+<?php
+** 
+* Plugin Name:       puctest
+* Plugin URI:        https://example.com/plugins/the-basics/
+* Description:       Handle the basics with this plugin.
+* Version:           1.0.0
