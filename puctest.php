73bc450579e8c8a59c48ac03af63406ceef76840
Jan Viehweger <janviehweger@users.noreply.github.com>
2020-12-11 15:58:06 +0100

    Update puctest.php
    fixed comment

--- puctest.php
+++ puctest.php
@@ -1,8 +1,9 @@
 <?php
-** 
+/** 
 * Plugin Name:       puctest
 * Description:       testing puc.
 * Version:           2.0.0
+*/
 
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
